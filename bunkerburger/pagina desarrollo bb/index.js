57f4acce78f315c14d97cf1ef7be8804a5d8a16c
jarodriguezca <aleandrorodriguez.123@hotmail.com>
2025-03-20 09:40:30 -0500

    Delete bunkerburger/pagina desarrollo bb directory

--- bunkerburger/pagina desarrollo bb/index.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.accordion-header').forEach(header => {
-    header.addEventListener('click', function() {
-        const content = this.nextElementSibling;
-        content.style.display = content.style.display === 'block' ? 'none' : 'block';
-    });
-}); 
